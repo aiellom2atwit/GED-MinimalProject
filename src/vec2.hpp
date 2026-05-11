bcbf2e055d65e019d62858c0d322a863e3a07a8a
Micah Schuster <micahdschuster@gmail.com>
2026-05-11 07:33:31 -0400

    Remove irrelevant code.

--- src/vec2.hpp
+++ src/vec2.hpp
@@ -19,14 +19,3 @@ class Vec2{
 
 Vec2 operator* (const float s, const Vec2& rhs);
 std::ostream& operator<< (std::ostream& os, const Vec2& v);
-
-struct rect{
-    float height;
-    float width;
-};
-
-template<typename T>
-class Shape{
-    public:
-        T size;
-};
